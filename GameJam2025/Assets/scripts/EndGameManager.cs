b003e95b03dea47b9f182e25ba031524dacc24d8
Ashley <ashleyclairetanton@icloud.com>
2025-07-12 10:59:33 +0200

    end screens

--- GameJam2025/Assets/scripts/EndGameManager.cs
+++ GameJam2025/Assets/scripts/EndGameManager.cs
@@ -6,16 +6,22 @@ public class EndGameManager : MonoBehaviour
 {
     public Timer timer;
     public FirstPersonControls firstPersonControls;
+
+    public GameObject winScreen;
+    public GameObject loseScreen;
+
     void Update()
     {
         if(timer.TimeRemanding <= 0) 
         {
             Debug.Log("EndGameFail");
+            loseScreen.SetActive(true);
         }
 
         if(firstPersonControls.PacientsRemanding <= 0 ) 
         {
             Debug.Log("EndGamesucess");
+            winScreen.SetActive(true);
         }
     }
 }
