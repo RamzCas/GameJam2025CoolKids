3bcf2b73fe5e5cc31a1d907a86bf6d7ed42faf94
RamzCas <2749414@students.wits.ac.za>
2025-07-12 10:19:13 +0200

    FOV Change

---
+++ GameJam2025/Assets/scripts/FOV Change.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FOVChange : MonoBehaviour
+{
+    public Timer Timer;
+    public Camera PlayerCam;
+    public float CurrentFOV;
+    public float MaxFOV;
+    public float MinFOV;
+
+
+    private void Update()
+    {
+        CurrentFOV = Timer.TimeRemanding;
+        PlayerCam.fieldOfView = Timer.TimeRemanding;
+
+
+        if(Timer.TimeRemanding >= MaxFOV) 
+        {
+            PlayerCam.fieldOfView = MaxFOV;    
+        }
+
+        if(Timer.TimeRemanding <= MinFOV) 
+        {
+            PlayerCam.fieldOfView = MinFOV;
+        }
+    }
+}
